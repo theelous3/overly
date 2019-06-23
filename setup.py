40ebdd43cdb6aba4c45226289381f9e911c169e2
theelous3 <mark.eric.jameson@gmail.com>
2019-06-23 22:55:05 +0100

    add package data param

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     description='overly - http client testing for hoomans',
     long_description='overly is for testing your http client, from the balan to the bananas.',
     license='MIT',
-    version='0.1.5',
+    version='0.1.6',
     python_requires=">=3.7",
     author='Mark Jameson - aka theelous3',
     url='https://github.com/theelous3/overly',
     packages=['overly'],
+    include_package_data=True,
     install_requires=['h11', 'sansio_multipart'],
     classifiers=[
         'Programming Language :: Python :: 3',
